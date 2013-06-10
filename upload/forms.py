d422ae42cd59a940ffd8f8df6b7ccec47884bc17
Jalcubo <jdcuadrado3@gmail.com>
2013-06-10 22:38:29 +0100

    Progreso con la memoria, añadidas imagenes de la seccion 3

--- upload/forms.py
+++ upload/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class DocumentForm(forms.Form):
     docfile = forms.FileField(
-        label='Select a file',
+        label='Select a file .csv',
         help_text='max. 42 megabytes'
     )
