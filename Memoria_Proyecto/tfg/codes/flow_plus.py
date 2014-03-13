95993687684e136b7890d3d791e14b9057fe68f3
steven <steven@steven-VirtualBox.(none)>
2014-03-13 11:26:45 +0000

    eliminando memoria del proyecto pasada

--- Memoria_Proyecto/tfg/codes/flow_plus.py
+++
@@ -1,9 +0,0 @@
-FLOW = OAuth2WebServerFlow (
-    client_id ='480482329789-dj...',
-    client_secret = 'C5umC7xa_ML704wnmdT79Bh0',
-    token_uri='https://accounts.google.com/o/oauth2/token',
-    scope= ['https://www.googleapis.com/auth/drive',
-            'https://www.googleapis.com/auth/plus.me'],
-    redirect_uri= 'http://localhost:8000/oauth2callback', 
-    access_type='offline',
-    approval_prompt='force')
